def8f1d3d48e2ce5f670a91c1bee7dd1aa032623
Ryan Mauldin <ryanmauldin@msn.com>
2016-05-24 12:31:17 -0400

    Adding StreamExtensions

---
+++ OpenShare.Net/OpenShare.Net.Library.Common/StreamExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OpenShare.Net.Library.Common
+{
+    public static class StreamExtensions
+    {
+        public static byte[] GetStreamBytes(this Stream stream)
+        {
+            var buffer = new byte[stream.Length];
+            stream.Read(buffer, 0, Convert.ToInt32(stream.Length));
+            return buffer;
+        }
+
+        public static async Task<byte[]> GetStreamBytesAsync(this Stream stream)
+        {
+            var buffer = new byte[stream.Length];
+            await stream.ReadAsync(buffer, 0, Convert.ToInt32(stream.Length));
+            return buffer;
+        }
+
+        public static byte[] GetStreamBytes(this Stream stream, int length)
+        {
+            var buffer = new byte[length];
+            stream.Read(buffer, 0, length);
+            return buffer;
+        }
+
+        public static async Task<byte[]> GetStreamBytesAsync(this Stream stream, int length)
+        {
+            var buffer = new byte[length];
+            await stream.ReadAsync(buffer, 0, length);
+            return buffer;
+        }
+    }
+}
