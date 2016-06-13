68379773f474e62d7c1688f19b6cb6e12aa405eb
Ryan Mauldin <ryanmauldin@msn.com>
2016-06-12 21:47:09 -0400

    Basic Fixes
    Basic Fixes

--- OpenShare.Net/OpenShare.Net.Library.Repository/IdentityKeys/EntityIdentityKeys.cs
+++ OpenShare.Net/OpenShare.Net.Library.Repository/IdentityKeys/EntityIdentityKeys.cs
@@ -10,41 +10,41 @@ namespace OpenShare.Net.Library.Repository.IdentityKeys
 
     public interface EntityIdentityKey_idAsInt32 : IEntityIdentity_idAsInt32
     {
-        int id { get; set; }
+        
     }
 
     public interface EntityIdentityKey_IDAsInt32 : IEntityIdentity_IDAsInt32
     {
-        int ID { get; set; }
+        
     }
 
     public interface EntityIdentityKey_IdAsString : IEntityIdentity_IdAsString
     {
-        string Id { get; set; }
+        
     }
 
     public interface EntityIdentityKey_idAsString : IEntityIdentity_idAsString
     {
-        string id { get; set; }
+        
     }
 
     public interface EntityIdentityKey_IDAsString : IEntityIdentity_IDAsString
     {
-        string ID { get; set; }
+        
     }
 
     public interface EntityIdentityKey_IdAsGuid : IEntityIdentity_IdAsGuid
     {
-        Guid Id { get; set; }
+        
     }
 
     public interface EntityIdentityKey_idAsGuid : IEntityIdentity_idAsGuid
     {
-        Guid id { get; set; }
+        
     }
 
     public interface EntityIdentityKey_IDAsGuid : IEntityIdentity_IDAsGuid
     {
-        Guid ID { get; set; }
+        
     }
 }
